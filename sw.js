57791e032b29b008e9545371a729a00f80abc4b2
tomcamau <kimgiahan20@gmail.com>
2026-08-15 17:23:39 +0700

    Update sw.js
    fix sw

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'socantom-lai-v1';
+const CACHE_NAME = 'socantom-lai-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
